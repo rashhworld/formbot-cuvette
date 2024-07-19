4395d22ab62c016002969412bc4b47d4f4c502d2
Rashh <ranjan.rashmi2016@outlook.com>
2024-07-19 13:11:25 +0530

    Basic setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+import Homepage from "./pages/Homepage"
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
