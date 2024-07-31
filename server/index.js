6f70aca934c4469f5af02c4ef587a34c64af723d
Rashmi Ranjan <rashhworld@hotmail.com>
2024-07-31 23:11:51 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,13 +13,13 @@ const errorHandler = require('./src/middlewares/errorHandler');
 const app = express();
 dotenv.config();
 
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+app.use(cors({
+    origin: 'https://formbot-cuvette.vercel.app', // Replace with your allowed origin
+    methods: 'GET, POST, PUT, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true
+}));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
